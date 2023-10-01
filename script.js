2a36a76607edd3a6bcaa372c63a10f8fb9d42b3a
Shivani Anandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-10-01 21:03:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
-
+function swapTheme(){
+	const toggle = "true";
+	if(toggle){
+		document.getElementById("app").classList.add("night");
+		document.getElementById("swap").classList.add("button_night");
+		toggle="false"
+	}
+	else{
+		document.getElementById("app").classList.remove("night");
+		document.getElementById("swap").classList.remove("button_night");
+	}
+}
